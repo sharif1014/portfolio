827c3365bd3851d568d731aace18ebad2f353b04
Sharif <sharifuliuk@gmail.com>
2022-08-26 01:02:36 +0600

    App file updated

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import About from "./pages/about/About";
-import Contact from './pages/contact/Contact';
-import Home from './pages/home/Home';
-import Services from './pages/services/Services';
-import Skills from './pages/skills/Skills';
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
+import Services from "./pages/services/Services";
+import Skills from "./pages/skills/Skills";
 
 function App() {
   return (
     <>
-      <Navbar/>
       <Router>
-          <Routes>
-          <Route exact path="/" element={ <Home />}>
-           
-          </Route>
-          <Route path="/about" element={  <About />}>
-           
-          </Route>
-          <Route path="/skills" element={ <Skills />}>
-          </Route>
-          <Route path="/services" element={ <Services />}>
-          
-          </Route>
-          <Route path="/contact-us" element={ <Contact />}>
-          </Route>
-          </Routes>  
+        <Navbar />
+        <Switch>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/skills" element={<Skills />}></Route>
+          <Route path="/services" element={<Services />}></Route>
+          <Route path="/contact-us" element={<Contact />}></Route>
+        </Switch>
+        <Footer />
       </Router>
-      <Footer/>
-     </>
-)}
+    </>
+  );
+}
 
 export default App;
